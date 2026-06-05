6cd6aa32fda072579c14231f23d3d61a20fac742
dimaskuy <dimasramadhansyaputhra09@gmail.com>
2026-06-05 22:15:15 +0700

    initial main menu

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
+
 import "fmt"
 
+func menu() int { // dimas - menambahkan fungsi menu
+	var input int
+	fmt.Println("1. Lihat bangunan")
+	fmt.Println("2. Update")
+	fmt.Println("3. Delete")
+	fmt.Println("4. Urutkan")
+	fmt.Println("5. Cari")
+	fmt.Print("Masukkan pilihan (1-5): ")
+
+	_, err := fmt.Scanln(&input)
+
+	if err != nil {
+		fmt.Println("Input harus berupa angka!")
+		return 0
+	}
+	return input
+}
 
 func main() {
-	fmt.Println("hello world!")
+	input := menu()
+	fmt.Print(input)
 }
